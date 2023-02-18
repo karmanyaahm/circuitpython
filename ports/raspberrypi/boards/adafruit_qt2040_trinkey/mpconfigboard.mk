d902e1be5704278492957e5c73e2d1c6ec5c0d39
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2023-02-18 16:36:16 -0600

    freeze libraries

--- ports/raspberrypi/boards/adafruit_qt2040_trinkey/mpconfigboard.mk
+++ ports/raspberrypi/boards/adafruit_qt2040_trinkey/mpconfigboard.mk
@@ -7,3 +7,5 @@ CHIP_VARIANT = RP2040
 CHIP_FAMILY = rp2
 
 EXTERNAL_FLASH_DEVICES = "W25Q64JVxQ"
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
